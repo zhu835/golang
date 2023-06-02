6440bb2d608e778fa6d49bf8e2721183cbb11ea0
zhu835 <zhumeng83377797@163.com>
2023-06-02 17:27:27 +0800

    v2

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+import "bytes"
+import "strings"
+
+func main() {
+	age := 19
+    name：= “tt”
+	//	var a1 = true
+
+	if age >= 18 {
+
+		fmt.Printf("ssssssss")
+		fmt.Printf("age: %v\n", age)
+
+	}
+var  buffer bytes.Buffer
+ buffer.WriteString("sss")
+
+ fmt.Fprint()
+
+}
